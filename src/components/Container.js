874c631232d8ca25a78a2e9c8accdd036902537e
krishna <krishgithub10@gmail.com>
2022-07-08 06:15:12 +0530

    added some content

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,8 +1,39 @@
-import Tools from "./Tools"
+import Tools from "./Tools";
 
 export default function Container() {
-    return(
-        <div>
-        </div>
-    );
+  return (
+    <div className="Container">
+      <h1 className="h11">Hey, I'm Krishna &#128075;</h1>
+      <p className="p1">
+        <span>
+          gm☀️! I am Krishna, a 15-year-old blockchain-cum-web developer from
+          India. <br></br> My main professionalism is in web development, but I
+          also have experience working with latest tech-stacks. <br></br> Apart
+          from web dev, I have learned blockchain development and worked with
+          big companies like Layer3 and Thirdweb by building smart contracts for
+          their projects using solidity. I am a part-time freelancer :)
+        </span>
+      </p>
+
+      <h2 className="h22">What I Do 💭</h2>
+
+      <p className="p2">
+        I'm passionate about everything technology; from designing and
+        developing software, to understanding how the many moving parts of the
+        internet work together, to cybersecurity, systems, programming, and so
+        much more. I strive to learn more about these things every day, and
+        utilize my knowledge to further understand how or why the technology
+        around us works.
+      </p>
+
+      <h2 className="h23">Technologies 💻</h2>
+
+      <p className="p3">
+        I use a variety of tools to streamline my development process and
+        increase the quality of both my code, and my projects. Below is a list
+        of technologies and languages I've had experience with in the past, or
+        use currently.
+      </p>
+    </div>
+  );
 }
