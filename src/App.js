7bd062146af9ee3069b3216135bb3c2a09138be6
krishna <krishgithub10@gmail.com>
2022-07-09 19:05:49 +0530

    fixed padding

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import logo from './logo.svg';
 import Main from './components/Main';
+import Contact from './Contact';
 import './App.css';
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <Main></Main>
-    </div>
+    <Router>
+      <div className="App">
+          <Navbar></Navbar>
+        <div className='content'>
+          <Routes>
+            <Route path="/portfolio" element={<Main />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,8 @@
-import Navbar from './Navbar';
 import Container from './Container';
 
 export default function Main() {
     return(
         <div className='Main'>
-            <Navbar></Navbar>
             <Container></Container>
         </div>
     );
